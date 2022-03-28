68aa5efbe40ef2e10c5a1eed8035191fa0056eef
Miller <farwes1180@gmail.com>
2022-03-28 17:07:21 +0900

    flex - align css property

--- flexbox/src/App.js
+++ flexbox/src/App.js
@@ -7,6 +7,9 @@ function App() {
         <li>3rd Child</li>
         <li>4th Child</li>
         <li>5th Child</li>
+        <li>6th Child</li>
+        <li>7th Child</li>
+        <li>8th Child</li>
       </ul>
     </div>
   );
