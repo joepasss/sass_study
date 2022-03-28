e34dab14db5bab533d15552eb3a21c2645d0019f
Miller <farwes1180@gmail.com>
2022-03-28 17:26:31 +0900

    grow property

--- flexbox/src/App.js
+++ flexbox/src/App.js
@@ -6,10 +6,10 @@ function App() {
         <li>2nd Child</li>
         <li>3rd Child</li>
         <li>4th Child</li>
-        <li>5th Child</li>
+        {/* <li>5th Child</li>
         <li>6th Child</li>
         <li>7th Child</li>
-        <li>8th Child</li>
+        <li>8th Child</li> */}
       </ul>
     </div>
   );
