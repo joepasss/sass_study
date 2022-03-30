d89453dc37f38a7f8d2e3a126c9d242d91e72590
Miller <farwes1180@gmail.com>
2022-03-30 17:49:08 +0900

    challenge 1 (before add design feature)

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,19 +1,56 @@
 function App() {
   return (
     <div className='container'>
-      <a href='#' className='logo'>
-        Some Logo
-      </a>
-      <h1>Some heading</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        obcaecati quisquam illum consequuntur quam id suscipit, libero ullam nam
-        aspernatur tempora reprehenderit at dolorem explicabo eligendi
-        consequatur dignissimos, facilis reiciendis!
-      </p>
-      <a href='#' className='cta'>
-        Sign Up
-      </a>
+      <div className='card dog'>
+        <img
+          src='https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg'
+          alt=''
+        />
+        <div className='contents'>
+          <p className='date'>Nov 10, 2100</p>
+          <h3>DOG</h3>
+          <p className='content'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
+
+      <div className='card moose'>
+        <img
+          src='https://i0.wp.com/wilderness-society.org/wp-content/uploads/2021/03/moose-70254.jpg'
+          alt=''
+        />
+        <div className='contents'>
+          <p className='date'>Nov 10, 2100</p>
+          <h3>Moose</h3>
+          <p className='content'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
+
+      <div className='card owl'>
+        <img
+          src='https://images.unsplash.com/photo-1543549790-8b5f4a028cfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JlYXQlMjBob3JuZWQlMjBvd2x8ZW58MHx8MHx8&w=1000&q=80'
+          alt=''
+        />
+        <div className='contents'>
+          <p className='date'>Nov 10, 2100</p>
+          <h3>Owl</h3>
+          <p className='content'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
