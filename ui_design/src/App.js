00f5789496707b2c2a30e3b56d4b034b7972473d
Miller <farwes1180@gmail.com>
2022-03-31 08:37:12 +0900

    typography ui design

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,58 +1,44 @@
 function App() {
   return (
-    // main#main-content>section*3.section.sections-$>img+div.content>p{Nov 15, 2100}+h3{Some Title}+p{Some Text}
-
-    <main id='main-content'>
-      <section className='section section-1'>
-        <img
-          src='https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>DOG</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
-      </section>
-
-      <section className='section section-2'>
-        <img
-          src='https://i0.wp.com/wilderness-society.org/wp-content/uploads/2021/03/moose-70254.jpg'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>Moose</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
-      </section>
-
-      <section className='section section-3'>
-        <img
-          src='https://www.allaboutbirds.org/guide/assets/photo/297366501-480px.jpg'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>Owl</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
-      </section>
+    <main>
+      <h1 id='main-heading'>Some Heading</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint adipisci
+      </p>
+      <div className='all-content'>
+        <section className='section section1'>
+          <div className='content'>
+            <img
+              src='https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/3:2/w_3329,h_2219,c_limit/1521-WIRED-Cat.jpeg'
+              alt=''
+            />
+            <p>This is really important topic about cats that must be read</p>
+            <p>My Cat</p>
+          </div>
+        </section>
+        <section className='section section2'>
+          <div className='content'>
+            <img
+              src='https://images.ctfassets.net/81iqaqpfd8fy/3r4flvP8Z26WmkMwAEWEco/870554ed7577541c5f3bc04942a47b95/78745131.jpg?w=1200&h=1200&fm=jpg&fit=fill'
+              alt=''
+            />
+            <p>
+              This is really important topic about giraffes that must be read
+            </p>
+            <p>My Giraffe</p>
+          </div>
+        </section>
+        <section className='section section3'>
+          <div className='content'>
+            <img
+              src='https://i.ytimg.com/vi/o0wRPiOwJ_g/maxresdefault.jpg'
+              alt=''
+            />
+            <p>This is really important topic about foxs that must be read</p>
+            <p>My Fox</p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
