ad4ada8f39a8fdb5a0b8a66bf78a5d632d5feddb
Miller <farwes1180@gmail.com>
2022-03-31 10:51:25 +0900

    git remote add

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,46 +1,5 @@
 function App() {
-  return (
-    <main>
-      <h1 id='main-heading'>Some Heading</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint adipisci
-      </p>
-      <div className='all-content'>
-        <section className='section section1'>
-          <div className='content'>
-            <img
-              src='https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/3:2/w_3329,h_2219,c_limit/1521-WIRED-Cat.jpeg'
-              alt=''
-            />
-            <p>This is really important topic about cats that must be read</p>
-            <p>My Cat</p>
-          </div>
-        </section>
-        <section className='section section2'>
-          <div className='content'>
-            <img
-              src='https://images.ctfassets.net/81iqaqpfd8fy/3r4flvP8Z26WmkMwAEWEco/870554ed7577541c5f3bc04942a47b95/78745131.jpg?w=1200&h=1200&fm=jpg&fit=fill'
-              alt=''
-            />
-            <p>
-              This is really important topic about giraffes that must be read
-            </p>
-            <p>My Giraffe</p>
-          </div>
-        </section>
-        <section className='section section3'>
-          <div className='content'>
-            <img
-              src='https://i.ytimg.com/vi/o0wRPiOwJ_g/maxresdefault.jpg'
-              alt=''
-            />
-            <p>This is really important topic about foxs that must be read</p>
-            <p>My Fox</p>
-          </div>
-        </section>
-      </div>
-    </main>
-  );
+  return <></>;
 }
 
 export default App;
