cdfbc58ac53c5b0765dff0bc2851322dab1f5378
Miller <farwes1180@gmail.com>
2022-03-31 11:17:49 +0900

    color ui

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <header></header>
+      <section>
+        <h1>Some Very Important Title</h1>
+        <p>
+          Some text that relates to above very important topic is supposed to
+          appear here
+        </p>
+        <a href='#'>Some link</a>
+      </section>
+    </>
+  );
 }
 
 export default App;
