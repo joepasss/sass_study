868d1deb61972f60202920530979fc547e76ad82
Miller <farwes1180@gmail.com>
2022-04-01 09:17:40 +0900

    app init

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,17 +1,5 @@
 function App() {
-  return (
-    <>
-      <header></header>
-      <section>
-        <h1>Some Very Important Title</h1>
-        <p>
-          Some text that relates to above very important topic is supposed to
-          appear here
-        </p>
-        <a href='#'>Some link</a>
-      </section>
-    </>
-  );
+  return <>APP</>;
 }
 
 export default App;
