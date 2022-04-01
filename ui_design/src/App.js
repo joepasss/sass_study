c0741b53daefd8c7bcac589661e8e973ef921dad
Miller <farwes1180@gmail.com>
2022-04-01 09:30:30 +0900

    alignment

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,35 +1,19 @@
 function App() {
   return (
-    <div className='white-space'>
-      <div className='sports football'>
-        <h1>Football</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
-          harum nemo minima molestias earum qui reprehenderit dignissimos amet
-          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
-          pariatur! Nostrum, voluptatum!
-        </p>
-      </div>
-
-      <div className='sports basketball'>
-        <h1>Basketball</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
-          harum nemo minima molestias earum qui reprehenderit dignissimos amet
-          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
-          pariatur! Nostrum, voluptatum!
-        </p>
-      </div>
-
-      <div className='sports golf'>
-        <h1>Golf</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
-          harum nemo minima molestias earum qui reprehenderit dignissimos amet
-          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
-          pariatur! Nostrum, voluptatum!
-        </p>
-      </div>
+    <div className='container'>
+      <a href='#' className='logo'>
+        Some Logo
+      </a>
+      <h1>Some heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero soluta
+        vitae incidunt totam architecto illum laboriosam nemo velit, voluptatem
+        enim eveniet quam dignissimos cumque magnam voluptates saepe, nobis
+        minima necessitatibus?
+      </p>
+      <a href='#' className='cta'>
+        Sign Up
+      </a>
     </div>
   );
 }
