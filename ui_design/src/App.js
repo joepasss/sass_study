5e1c427ec15b0a137da4c7dc201a3fe470a1eb67
Miller <farwes1180@gmail.com>
2022-04-01 09:22:09 +0900

    white space

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,5 +1,37 @@
 function App() {
-  return <>APP</>;
+  return (
+    <div className='white-space'>
+      <div className='sports football'>
+        <h1>Football</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          harum nemo minima molestias earum qui reprehenderit dignissimos amet
+          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
+          pariatur! Nostrum, voluptatum!
+        </p>
+      </div>
+
+      <div className='sports basketball'>
+        <h1>Basketball</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          harum nemo minima molestias earum qui reprehenderit dignissimos amet
+          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
+          pariatur! Nostrum, voluptatum!
+        </p>
+      </div>
+
+      <div className='sports golf'>
+        <h1>Golf</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          harum nemo minima molestias earum qui reprehenderit dignissimos amet
+          facere, blanditiis dolor quam aperiam minus doloremque sit nesciunt
+          pariatur! Nostrum, voluptatum!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
