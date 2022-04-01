ba16cc525e51ed3105cf5f40ab0219133aa357c8
Miller <farwes1180@gmail.com>
2022-04-01 09:39:44 +0900

    challenge 1

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,19 +1,56 @@
 function App() {
   return (
     <div className='container'>
-      <a href='#' className='logo'>
-        Some Logo
-      </a>
-      <h1>Some heading</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero soluta
-        vitae incidunt totam architecto illum laboriosam nemo velit, voluptatem
-        enim eveniet quam dignissimos cumque magnam voluptates saepe, nobis
-        minima necessitatibus?
-      </p>
-      <a href='#' className='cta'>
-        Sign Up
-      </a>
+      <div className='card dog'>
+        <img
+          src='https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg'
+          alt=''
+        />
+        <div className='contents'>
+          <p>Nov 10, 2100</p>
+          <h3>DOG</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
+
+      <div className='card moose'>
+        <img
+          src='https://i0.wp.com/wilderness-society.org/wp-content/uploads/2021/03/moose-70254.jpg'
+          alt=''
+        />
+        <div className='contents'>
+          <p>Nov 10, 2100</p>
+          <h3>Moose</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
+
+      <div className='card owl'>
+        <img
+          src='https://assets.technologynetworks.com/production/dynamic/images/content/342327/unique-dna-packaging-in-owl-eyes-may-be-a-lens-that-enhances-night-vision-342327-1280x720.jpg?cb=10962144'
+          alt=''
+        />
+        <div className='contents'>
+          <p>Nov 10, 2100</p>
+          <h3>Owl</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+            laborum possimus dolore non illo sint, eveniet nihil labore quidem
+            magnam quo consequatur et minima blanditiis impedit debitis, neque,
+            hic ea?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
