5c391a95f024dd1e0f3d69e81b5e4bb9065d5bf2
Miller <farwes1180@gmail.com>
2022-04-01 10:14:18 +0900

    color

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,57 +1,16 @@
 function App() {
   return (
-    <main id='main-content'>
-      <section className='section section-1'>
-        <img
-          src='https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>DOG</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
+    <>
+      <header></header>
+      <section>
+        <h1>Some Very Important Title</h1>
+        <p>
+          Some text that relates to above very important topic is supposed to
+          appear here
+        </p>
+        <a href='#'>Some link</a>
       </section>
-
-      <section className='section section-2'>
-        <img
-          src='https://i0.wp.com/wilderness-society.org/wp-content/uploads/2021/03/moose-70254.jpg'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>Moose</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
-      </section>
-
-      <section className='section section-3'>
-        <img
-          src='https://assets.technologynetworks.com/production/dynamic/images/content/342327/unique-dna-packaging-in-owl-eyes-may-be-a-lens-that-enhances-night-vision-342327-1280x720.jpg?cb=10962144'
-          alt=''
-        />
-        <div className='contents'>
-          <p>Nov 10, 2100</p>
-          <h3>Owl</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
-            laborum possimus dolore non illo sint, eveniet nihil labore quidem
-            magnam quo consequatur et minima blanditiis impedit debitis, neque,
-            hic ea?
-          </p>
-        </div>
-      </section>
-    </main>
+    </>
   );
 }
 
