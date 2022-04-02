c131fa0b346ece0ec627d4ed92b55a12f2361ce9
Miller <farwes1180@gmail.com>
2022-04-02 23:15:19 +0900

    challenge 2 init

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,16 +1,21 @@
 function App() {
   return (
-    <>
-      <header></header>
-      <section>
-        <h1>Some Very Important Title</h1>
-        <p>
-          Some text that relates to above very important topic is supposed to
-          appear here
-        </p>
-        <a href='#'>Some link</a>
-      </section>
-    </>
+    <section>
+      <h1>Contact Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In laborum,
+        animi corporis possimus natus fuga!
+      </p>
+      <form>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' name='' id='name' />
+
+        <label htmlFor='message'>Your Message</label>
+        <textarea id='message' rows='15'></textarea>
+
+        <input type='submit' value='Send message' />
+      </form>
+    </section>
   );
 }
 
