7cfc19b29c0c81ef00caae918ca57065f9671552
Miller <farwes1180@gmail.com>
2022-04-03 01:07:02 +0900

    challenge 3 init

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,22 +1,31 @@
 function App() {
   return (
     <>
-      {/* <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section> */}
-
-      <section>
-        <h1>Join our Newsletter</h1>
-        <form>
-          <label for='email'>Email Address</label>
-          <div class='input-container'>
-            <input type='email' />
-            <input type='submit' value='Submit email' />
-          </div>
-        </form>
-      </section>
+      <aside></aside>
+      <main id='main-content'>
+        <h1>Movies</h1>
+        <section className='content'>
+          <h3>Favorite Movies</h3>
+          <ul>
+            <li>
+              <span>Winter Soldier</span>
+              <span>9.5</span>
+            </li>
+            <li>
+              <span>Aquaman</span>
+              <span>7.4</span>
+            </li>
+            <li>
+              <span>Endgame</span>
+              <span>9.8</span>
+            </li>
+            <li>
+              <span>Justice League</span>
+              <span>4.4</span>
+            </li>
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
