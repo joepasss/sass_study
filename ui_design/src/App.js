912fb1ff818b263dc5f51a3c3923b627d2434841
Miller <farwes1180@gmail.com>
2022-04-03 00:54:07 +0900

    visual hierarchy 02 init

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,11 +1,22 @@
 function App() {
   return (
     <>
+      {/* <section></section>
       <section></section>
       <section></section>
       <section></section>
-      <section></section>
-      <section></section>
+      <section></section> */}
+
+      <section>
+        <h1>Join our Newsletter</h1>
+        <form>
+          <label for='email'>Email Address</label>
+          <div class='input-container'>
+            <input type='email' />
+            <input type='submit' value='Submit email' />
+          </div>
+        </form>
+      </section>
     </>
   );
 }
