c803de026341352cf5958d62695b7c26184d53a0
Miller <farwes1180@gmail.com>
2022-04-04 07:20:44 +0900

    visual hierarchy 01

--- ui_design/src/App.js
+++ ui_design/src/App.js
@@ -1,21 +1,12 @@
 function App() {
   return (
-    <section>
-      <h1>Contact Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In laborum,
-        animi corporis possimus natus fuga!
-      </p>
-      <form>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' name='' id='name' />
-
-        <label htmlFor='message'>Your Message</label>
-        <textarea id='message' rows='15'></textarea>
-
-        <input type='submit' value='Send message' />
-      </form>
-    </section>
+    <>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
   );
 }
 
