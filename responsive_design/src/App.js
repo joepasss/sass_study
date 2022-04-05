857bdcd38460db7b8200d7ee93bdd66f91ece07b
Miller <farwes1180@gmail.com>
2022-04-06 04:02:17 +0900

    media queries

--- responsive_design/src/App.js
+++ responsive_design/src/App.js
@@ -1,18 +1,8 @@
+import './App.scss';
+import './AppOver400.scss';
+
 function App() {
-  return (
-    <div>
-      <img
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg'
-        alt='cat'
-        className='cat'
-      />
-      <img
-        src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/how-to-keep-ducks-call-ducks-1615457181.jpg?resize=640:*'
-        alt='duck'
-        className='duck'
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
