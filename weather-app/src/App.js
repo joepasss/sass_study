3ec6cd657c9386d5d6d4909b4eb3fdf28c06c60f
Miller <farwes1180@gmail.com>
2022-04-08 18:58:13 +0900

    Weather app 01

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,35 @@
+import { FaSun } from 'react-icons/fa';
+
 function App() {
-  return <div>asd</div>;
+  return (
+    <div>
+      <section id='weather'>
+        <h1>Sydney, Australia</h1>
+        {/* Weather info */}
+        <div className='weather__info__temp'>
+          <div className='weather__info'>
+            <div className='weather__info--date'>
+              <p>Thursday, Apr 30</p>
+              <p>Sunny</p>
+              <div className='weather__info--date__icon__temp'>
+                <FaSun className='icons' />
+                <p>
+                  24 <sup>&#8451;</sup>
+                </p>
+              </div>
+            </div>
+            <div className='weather__info--data'>
+              <p>Percipitation: 10%</p>
+              <p>Humidity: 5%</p>
+              <p>Wind: 4 kpm NW</p>
+            </div>
+          </div>
+
+          {/* Weather temp */}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
