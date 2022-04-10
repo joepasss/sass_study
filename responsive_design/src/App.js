78b37381ea2a0a32beb663d2ee407c8b89dce272
Miller <farwes1180@gmail.com>
2022-04-10 11:20:17 +0900

    Sticky Footer

--- responsive_design/src/App.js
+++ responsive_design/src/App.js
@@ -1,12 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Full Screen and Viewport Units</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        voluptatum dolorem eveniet consequuntur? Nisi quos
-      </p>
-      <a href='#'>Sign Up</a>
+      <header>Header</header>
+      <section>Section</section>
+      <footer>Footer</footer>
     </div>
   );
 }
