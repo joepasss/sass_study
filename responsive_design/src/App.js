5b41b60c1d0da63d7521c8148cff5ba38a1e7104
Miller <farwes1180@gmail.com>
2022-04-10 10:50:17 +0900

    Full height

--- responsive_design/src/App.js
+++ responsive_design/src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
     <div>
-      <h1>Font Size and Viewport Units</h1>
+      <h1>Full Screen and Viewport Units</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        voluptatum dolorem eveniet consequuntur? Nisi quos
+      </p>
+      <a href='#'>Sign Up</a>
     </div>
   );
 }
