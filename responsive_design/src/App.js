3ec243a36ffd5147819ae94b8b079996fc2b8777
Miller <farwes1180@gmail.com>
2022-04-10 11:27:46 +0900

    Margins & Paddings

--- responsive_design/src/App.js
+++ responsive_design/src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div>
-      <header>Header</header>
-      <section>Section</section>
-      <footer>Footer</footer>
+    <div className='app'>
+      <h1>Fitness Matters</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ad eius
+        fugit maxime obcaecati praesentium pariatur nobis. Incidunt
+      </p>
     </div>
   );
 }
