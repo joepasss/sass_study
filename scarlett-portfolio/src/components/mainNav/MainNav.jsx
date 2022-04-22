a93f7717ee7c1242eb1cfc558d1bb4dc4528f8b1
Miller <farwes1180@gmail.com>
2022-04-22 18:38:29 +0900

    Main Nav 03

--- scarlett-portfolio/src/components/mainNav/MainNav.jsx
+++ scarlett-portfolio/src/components/mainNav/MainNav.jsx
@@ -1,34 +1,44 @@
 import './mainNav.scss';
 
+import { useState } from 'react';
+
 function MainNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [fade, setFade] = useState(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+    if (!fade) {
+      setTimeout(setFade(true), 5000);
+    } else {
+      setFade(false);
+    }
+  };
+
+  const mainNavList = [
+    'home',
+    'About',
+    'Projects',
+    'Services',
+    'Hire Me!',
+    'Contact',
+  ];
+
   return (
     <header id='header'>
-      <nav className='header__main-nav'>
-        <div className='header__main-nav--hamburger'>
+      <nav id='header__main-nav' className={isOpen ? 'open' : ''}>
+        <div id='header__main-nav--hamburger' onClick={onClick}>
           <div className='line line1'></div>
           <div className='line line2'></div>
           <div className='line line3'></div>
         </div>
 
         <ul className='header__main--links'>
-          <li>
-            <a href='#!'>home</a>
-          </li>
-          <li>
-            <a href='#!'>About</a>
-          </li>
-          <li>
-            <a href='#!'>Projects</a>
-          </li>
-          <li>
-            <a href='#!'>Services</a>
-          </li>
-          <li>
-            <a href='#!'>Hire Me!</a>
-          </li>
-          <li>
-            <a href='#!'>Contact</a>
-          </li>
+          {mainNavList.map((text, idx) => (
+            <li key={idx} className={fade ? 'fade' : ''}>
+              <a href='#!'>{text}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
