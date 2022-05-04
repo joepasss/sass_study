37d018cabbb42aa4f5e05e149ed2762d0d512baa
Miller <farwes1180@gmail.com>
2022-05-04 11:16:57 +0900

    CSS Grid Layout Introduction

--- grid-layout/src/App.tsx
+++ grid-layout/src/App.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
 
+import { GridContainer } from './sections/gridContainer/GridContainer';
+
 export const App: FC = () => {
-  return <div className='App'>GRID_APP</div>;
+  return (
+    <div className='App'>
+      <GridContainer />
+    </div>
+  );
 };
 
 export default App;
---
+++ grid-layout/src/sections/gridContainer/GridContainer.tsx
@@ -0,0 +1,19 @@
+import './gridContainer.scss';
+import { FC } from 'react';
+
+export const GridContainer: FC = () => {
+  return (
+    <section className='grid-container'>
+      <div className='item item1'>item-1</div>
+      <div className='item item2'>item-2</div>
+      <div className='item item3'>item-3</div>
+      <div className='item item4'>item-4</div>
+      <div className='item item5'>item-5</div>
+      <div className='item item6'>item-6</div>
+      <div className='item item7'>item-7</div>
+      <div className='item item8'>item-8</div>
+      <div className='item item9'>item-9</div>
+      <div className='item item10'>item-10</div>
+    </section>
+  );
+};
