a4ac0eaab15030edd3f651baf4aa677c4638d318
Miller <farwes1180@gmail.com>
2022-05-16 01:10:51 +0900

    The Auto-Fill & Auto-Fit Values

--- grid-layout/src/sections/gridContainer/GridContainer.tsx
+++ grid-layout/src/sections/gridContainer/GridContainer.tsx
@@ -5,24 +5,13 @@ import { FC } from 'react';
 export const GridContainer: FC = () => {
   return (
     <section className='grid-container'>
-      <div className='item item-1'>I</div>
-      <div className='item item-2'>was</div>
-      {/* <div className='item item-3'>written</div> */}
-      <div className='item item-3'>written or shown</div>
-      <div className='item item-4'>here</div>
-      <div className='item item-5'>to</div>
-      <div className='item item-6'>have</div>
-      {/* <div className='item item-7'>some</div> */}
-      <div className='item item-7'>something messed up here </div>
-      {/* <div className='item item-7'>
-        somethingmesseduphere{' '}
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-          nesciunt non, laborum ex aspernatur ipsam et sed repudiandae
-          distinctio praesentium!
-        </p>
-      </div> */}
-      <div className='item item-8'>fun</div>
+      <div className='item item-1'>item-1</div>
+      <div className='item item-2'>item-2</div>
+      <div className='item item-3'>item-3</div>
+      <div className='item item-4'>item-4</div>
+      <div className='item item-5'>item-5</div>
+      <div className='item item-6'>item-6</div>
+      <div className='item item-7'>item-7</div>
     </section>
   );
 };
