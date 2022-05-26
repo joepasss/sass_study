cf627f151e09224865b05c70f98b14699ae88083
Miller <farwes1180@gmail.com>
2022-05-26 11:51:07 +0900

    Layout

--- vlv/src/sections/events/Events.tsx
+++ vlv/src/sections/events/Events.tsx
@@ -3,7 +3,7 @@ import './events.scss';
 export const Events = () => {
   return (
     <>
-      <section id='event-content'>Event Content</section>
+      <section id='events-content'>Event Content</section>
       <section id='events-text'>Event Text</section>
     </>
   );
