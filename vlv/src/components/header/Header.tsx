6d3e9694ea5151f9009dd3b333f663d2bcea91e9
Miller <farwes1180@gmail.com>
2022-05-28 23:26:28 +0900

    Navigation component

--- vlv/src/components/header/Header.tsx
+++ vlv/src/components/header/Header.tsx
@@ -1,6 +1,12 @@
 import './header.scss';
 import { FC } from 'react';
 
+import { Navigation } from '../navigation/Navigation';
+
 export const Header: FC = () => {
-  return <header id='header'>HEADER</header>;
+  return (
+    <header id='header'>
+      <Navigation />
+    </header>
+  );
 };
