c39148ba14743a8cc143378d4562b294148f73df
Miller <farwes1180@gmail.com>
2022-05-28 12:11:53 +0900

    grid alignment (justify-items, align-items, place-items)

--- grid-layout/src/sections/gridContainer/GridContainer.tsx
+++ grid-layout/src/sections/gridContainer/GridContainer.tsx
@@ -4,13 +4,14 @@ import { FC } from 'react';
 export const GridContainer: FC = () => {
   return (
     <section className='grid-container'>
-      <div className='item item-1'>item-1</div>
-      <div className='item item-2'>item-2</div>
-      <div className='item item-3'>item-3</div>
-      <div className='item item-4'>item-4</div>
-      <div className='item item-5'>item-5</div>
-      <div className='item item-6'>item-6</div>
-      <div className='item item-7'>item-7</div>
+      <div className='item item-1'>item</div>
+      <div className='item item-2'>item</div>
+      <div className='item item-3'>item</div>
+      <div className='item item-4'>item</div>
+      <div className='item item-5'>item</div>
+      <div className='item item-6'>item</div>
+      <div className='item item-7'>item</div>
+      <div className='item item-8'>item</div>
     </section>
   );
 };
