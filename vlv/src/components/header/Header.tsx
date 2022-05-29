7805fa469b37bd10006de3c0865ed7aa24bcfb8c
Miller <farwes1180@gmail.com>
2022-05-29 15:56:58 +0900

    Build Header (Hero) Section

--- vlv/src/components/header/Header.tsx
+++ vlv/src/components/header/Header.tsx
@@ -7,6 +7,21 @@ export const Header: FC = () => {
   return (
     <header id='header'>
       <Navigation />
+      <div className='wrapper'>
+        <a href='#' className='logo'>
+          <span>V</span>L<span>V</span>
+        </a>
+
+        <div className='content'>
+          <h1 className='content-title'>Relax Your Soul</h1>
+          <p className='content-para'>
+            The team of VLV welcomes you. Start relaxing your soul and enjoy
+          </p>
+          <a href='#' className='content-link'>
+            Discover More
+          </a>
+        </div>
+      </div>
     </header>
   );
 };
