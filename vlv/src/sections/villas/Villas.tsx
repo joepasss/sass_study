0e58daad64b5b8aa0b54de4fa70cbbb58216c6bc
Miller <farwes1180@gmail.com>
2022-06-03 23:42:10 +0900

    Villas Section (desktop version)

--- vlv/src/sections/villas/Villas.tsx
+++ vlv/src/sections/villas/Villas.tsx
@@ -2,5 +2,73 @@ import './villas.scss';
 import { FC } from 'react';
 
 export const Villas: FC = () => {
-  return <section id='villas'>Villas</section>;
+  return (
+    <section id='villas'>
+      <div className='villa villas-1'>
+        <h2 className='villa-title'>Standard Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $200.00
+        </a>
+      </div>
+
+      <div className='villa villas-2'>
+        <h2 className='villa-title'>Superier Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $300.00
+        </a>
+      </div>
+
+      <div className='villa villas-3'>
+        <h2 className='villa-title'>Infinity Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $700.00
+        </a>
+      </div>
+
+      <div className='villa villas-4'>
+        <h2 className='villa-title'>Ultra Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $1100.00
+        </a>
+      </div>
+
+      <div className='villa villas-5'>
+        <h2 className='villa-title'>Galactic Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $1300.00
+        </a>
+      </div>
+
+      <div className='villa villas-6'>
+        <h2 className='villa-title'>Cosmic Villa</h2>
+        <p className='villa-para'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          perspiciatis!
+        </p>
+        <a href='#' className='villa-link'>
+          Starting at $2000.00
+        </a>
+      </div>
+    </section>
+  );
 };
