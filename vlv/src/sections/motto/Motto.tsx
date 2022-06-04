f74509f3fc2454cd91a0d3345c742615b9cacf37
Miller <farwes1180@gmail.com>
2022-06-04 14:27:35 +0900

    Motto Section

--- vlv/src/sections/motto/Motto.tsx
+++ vlv/src/sections/motto/Motto.tsx
@@ -1,5 +1,17 @@
 import './motto.scss';
 
 export const Motto = () => {
-  return <section id='motto'>Motto</section>;
+  return (
+    <section id='motto'>
+      <div className='content'>
+        <h2 className='motto-title'>We Reimagined Luxury</h2>
+        <p className='motto-para'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, unde!
+          Neque, impedit commodi dolores at dignissimos cumque expedita corrupti
+          laudantium, optio modi eos accusantium earum deleniti aliquam iure?
+          Itaque mollitia dignissimos perferendis autem obcaecati. Magnam.
+        </p>
+      </div>
+    </section>
+  );
 };
