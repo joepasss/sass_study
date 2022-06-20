8b12b392b20919aaf27c4e5c6548d5d0a7b1c1b3
Miller <farwes1180@gmail.com>
2022-06-21 04:05:23 +0900

    Features Section

--- vlv/src/sections/features/Features.tsx
+++ vlv/src/sections/features/Features.tsx
@@ -3,9 +3,31 @@ import './features.scss';
 export const Features = () => {
   return (
     <>
-      <section id='features-content'>Features Content</section>
+      <section id='features-content'>
+        <div className='feature-1'>
+          <h2 className='title'>Cocktail Package</h2>
+          <p className='para'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut officia
+            perferendis dolorem quos illum reiciendis at exercitationem
+            architecto ullam minus.
+          </p>
+        </div>
 
-      <section id='features-text'>Features Text</section>
+        <div className='feature-2'>
+          <h2 className='title'>Surfing Package</h2>
+          <p className='para'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut officia
+            perferendis dolorem quos illum reiciendis at exercitationem
+            architecto ullam minus.
+          </p>
+        </div>
+      </section>
+
+      <section id='features-text'>
+        <div className='content'>
+          <h2 className='title'>Features</h2>
+        </div>
+      </section>
     </>
   );
 };
