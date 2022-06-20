b0825cc11539e779f1a3db16f380ec88823ea3f7
Miller <farwes1180@gmail.com>
2022-06-21 06:24:08 +0900

    Testimonials Section

--- vlv/src/sections/testimonials/Testimonials.tsx
+++ vlv/src/sections/testimonials/Testimonials.tsx
@@ -1,5 +1,79 @@
 import './testimonials.scss';
 
 export const Testimonials = () => {
-  return <section id='testimonials'>Testimonials</section>;
+  return (
+    <section id='testimonials'>
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='front'>
+          <h2>Clark Lois</h2>
+        </div>
+        <div className='back'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            vitae.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
