a6edf4434536a24c89b14e7d9e40208d9c76cdd3
Miller <farwes1180@gmail.com>
2022-06-24 01:32:44 +0900

    footer section

--- vlv/src/components/footer/Footer.tsx
+++ vlv/src/components/footer/Footer.tsx
@@ -1,5 +1,19 @@
 import './footer.scss';
+import { FaGithub, FaDribbble } from 'react-icons/fa';
 
 export const Footer = () => {
-  return <footer id='footer'>Footer</footer>;
+  return (
+    <footer id='footer'>
+      <p>All Rights Reserved. &copy; VLV</p>
+      <div className='social'>
+        <a href='#!' className='social-link'>
+          <FaGithub className='icon' />
+        </a>
+
+        <a href='#!' className='social-link'>
+          <FaDribbble className='icon' />
+        </a>
+      </div>
+    </footer>
+  );
 };
