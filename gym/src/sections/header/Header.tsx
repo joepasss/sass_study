a9cc1b07c9a86c541200a3b18f29511b00450b09
Miller <farwes1180@gmail.com>
2022-07-24 02:14:09 +0900

    Create header markup

--- gym/src/sections/header/Header.tsx
+++ gym/src/sections/header/Header.tsx
@@ -1,5 +1,65 @@
 import { FC } from 'react';
 
+import logo from '../../img/logo/logo.svg';
+
 export const Header: FC = () => {
-  return <header className='header'>HEADER</header>;
+  return (
+    <header className='header'>
+      {/* Hamburger lines */}
+      <div className='header__menu'>
+        <div className='header__menu--line'></div>
+      </div>
+
+      {/* Overlay */}
+      <div className='header__overlay'></div>
+
+      <nav className='header__nav'>
+        <a href='#!' className='header__nav--log'>
+          <img src={logo} alt='LOGO' />
+        </a>
+
+        <ul className='header__nav--links'>
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='Home'>
+              Home
+            </a>
+          </li>
+
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='About'>
+              About
+            </a>
+          </li>
+
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='Classes'>
+              Classes
+            </a>
+          </li>
+
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='Events'>
+              Events
+            </a>
+          </li>
+
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='News'>
+              News
+            </a>
+          </li>
+
+          <li className='header__nav--links__item'>
+            <a href='#!' data-text='Shop'>
+              Shop
+            </a>
+          </li>
+
+          <div className='header__nav--links__toggle-container'>
+            <input type='checkbox' id='toggle' name='theme' />
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 };
