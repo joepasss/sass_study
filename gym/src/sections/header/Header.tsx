4c12f713d57f964942475f50f4272cc9ea17923b
Miller <farwes1180@gmail.com>
2022-07-25 02:05:27 +0900

    Add menu toggle function

--- gym/src/sections/header/Header.tsx
+++ gym/src/sections/header/Header.tsx
@@ -1,20 +1,31 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import logo from '../../img/logo/logo.svg';
 
 export const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className='header'>
+    <header className={isOpen ? 'header open' : 'header'}>
       {/* hamburger lines */}
-      <div className='header__menu'>
+      <div
+        className={isOpen ? 'header__menu open' : 'header__menu'}
+        onClick={() => toggle()}
+      >
         <div className='header__menu--line'></div>
       </div>
 
       {/* Overlay */}
-      <div className='header__overlay'></div>
+      <div
+        className={isOpen ? 'header__overlay open' : 'header__overlay'}
+      ></div>
 
       {/* Navigation */}
-      <nav className='header__nav'>
+      <nav className={isOpen ? 'header__nav open' : 'header__nav'}>
         <a href='!#' className='header__nav--logo'>
           <img src={logo} alt='logo' />
         </a>
