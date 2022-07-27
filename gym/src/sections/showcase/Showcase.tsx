eea1f379e0830f113d82405ab4373e93751f1452
Miller <farwes1180@gmail.com>
2022-07-27 11:57:53 +0900

    Markup showcase section

--- gym/src/sections/showcase/Showcase.tsx
+++ gym/src/sections/showcase/Showcase.tsx
@@ -1,5 +1,19 @@
 import { FC } from 'react';
 
 export const Showcase: FC = () => {
-  return <section className='showcase'>Showcase</section>;
+  return (
+    <section className='showcase'>
+      <div className='showcase__content'>
+        <h1 className='showcase__content--title'>Commit to be fit</h1>
+        <p className='showcase__content--para'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
+          harum!
+        </p>
+        <div className='showcase__content--links'>
+          <a href='#!'>Find out more</a>
+          <a href='#!'>Join Now</a>
+        </div>
+      </div>
+    </section>
+  );
 };
