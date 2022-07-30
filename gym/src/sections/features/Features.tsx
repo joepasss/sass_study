6e8f16bc1902a590e61438df33a073c6736a28a1
Miller <farwes1180@gmail.com>
2022-07-31 05:53:06 +0900

    features section markup

--- gym/src/sections/features/Features.tsx
+++ gym/src/sections/features/Features.tsx
@@ -1,5 +1,54 @@
 import { FC } from 'react';
 
 export const Features: FC = () => {
-  return <section className='features'>Features</section>;
+  const featureContent = [
+    {
+      title: 'Progression',
+    },
+    {
+      title: 'Power Yoga',
+    },
+    {
+      title: 'Workout',
+    },
+    {
+      title: 'Efficiency',
+    },
+    {
+      title: 'Nutrition',
+    },
+    {
+      title: 'Cardio Program',
+    },
+    {
+      title: 'Pure Strength',
+    },
+    {
+      title: 'Functionnality',
+    },
+    {
+      title: 'True Challenge',
+    },
+  ];
+
+  return (
+    <section className='features'>
+      {featureContent.map((item) => (
+        <div className='features__content'>
+          <h2 className='features__content--title'>{item.title}</h2>
+          <p className='features__content--para'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+            molestiae?
+          </p>
+          <a href='#!' className='features__content--lik'>
+            Read More
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+      ))}
+    </section>
+  );
 };
